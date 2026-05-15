8a1eab2e8869557a90d623bd90acb01f5f0877d0
hassanjk <1910213522@ogrenci.karabuk.edu.tr>
2026-05-16 00:05:01 +0300

    updates

--- Hero section design fidelity/src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-import Container from "../imports/Container/Container";
-
-export default function App() {
-  return (
-    <div className="size-full bg-black overflow-x-hidden">
-      <Container />
-    </div>
-  );
-}
